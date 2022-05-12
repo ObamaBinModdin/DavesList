e74678f1629d102cce83b7fa778074022cb99417
ObamaBinModdin <52817466+ObamaBinModdin@users.noreply.github.com>
2022-05-12 09:42:54 -0700

    Raw python code to help program.

---
+++ davesList/helperFunctions/functions.py
@@ -0,0 +1,9 @@
+import re
+
+def validateEmail(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    if (re.fullmatch(regex, email)):
+        return True
+
+    return False
